3084effbe3c92781d056b0628ce71f09e11af871
Andy Herzog <asherzog19@gmail.com>
2017-03-14 18:01:13 -0600

    DB connections

---
+++ myApp/routes/index.js
@@ -0,0 +1,149 @@
+let d3 = require('d3');
+let express = require('express');
+let xlsxj = require("xlsx-to-json");
+let router = express.Router();
+var alasql = require('alasql');
+var jsonQuery = require('json-query')
+var fs = require('fs'),
+  JSONStream = require('JSONStream'),
+  es = require('event-stream');
+const db = require('monk')('localhost/testDB');
+const wells = db.get('wells');
+const Pdp = db.get('pdp');
+const TC1 = db.get('typeCurve1');
+const TC2 = db.get('typeCurve2');
+const TC3 = db.get('typeCurve3');
+
+// let wells = [];
+router.get('/wells', function(req, res, next) {
+  wells.count({}).then(response => console.log(response));
+  return wells.find({})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
+router.get('/wells/:id', function(req, res, next) {
+  return wells.find({_id: req.params.id})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
+router.post('/wells', (req, res, next) => {
+  let data = req.body;
+  return wells.insert(data)
+    .then(result => {
+      res.json(result);
+    }).catch((err) => next(err));
+});
+
+router.patch('/wells/:id', (req, res, next) => {
+  return wells.update({_id: req.params.id}, {$set: req.body})
+    .then(result => {
+      res.json(result);
+    }).catch((err) => next(err));
+});
+
+router.delete('/wells/:id', (req, res, next) => {
+  return wells.findOneAndDelete({_id: req.params.id})
+    .then((response) => {
+      res.json({
+        response,
+        message: '🗑'
+      });
+    }).catch((err) => next(err));
+});
+
+router.get('/pdp', function(req, res, next) {
+  Pdp.count({}).then(response => console.log(response));
+  return Pdp.find({})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+router.get('/tc1', function(req, res, next) {
+  TC1.count({}).then(response => console.log(response));
+  return TC1.find({})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+router.get('/tc2', function(req, res, next) {
+  TC2.count({}).then(response => console.log(response));
+  return TC2.find({})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+router.get('/tc3', function(req, res, next) {
+  TC3.count({}).then(response => console.log(response));
+  return TC3.find({})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
+router.get('/day/:id', function(req, res, next) {
+  TC1.find({"Days" : +req.params.id}).then((result) => {
+    let all = [result];
+    TC2.find({"Days" : +req.params.id}).then((result2) => {
+      all.push(result2);
+      TC3.find({"Days" : +req.params.id}).then((result3) => {
+        all.push(result3);
+        res.json(all);
+      });
+    });
+  }).catch((err) => next(err));
+});
+
+router.get('/water/:id', function(req, res, next) {
+  return wells.find({"WATER_SYSTEM" : req.params.id})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
+router.get('/rig/:id', function(req, res, next) {
+  return wells.find({"RIG" : `Rig ${req.params.id}`})
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
+router.patch('/tc1/:id', (req, res, next) => {
+  return TC1.update({_id: req.params.id}, {$set: req.body})
+    .then(result => {
+      res.json(result);
+    }).catch((err) => next(err));
+});
+
+router.patch('/tc2/:id', (req, res, next) => {
+  return TC2.update({_id: req.params.id}, {$set: req.body})
+    .then(result => {
+      res.json(result);
+    }).catch((err) => next(err));
+});
+
+router.patch('/tc3/:id', (req, res, next) => {
+  return TC3.update({_id: req.params.id}, {$set: req.body})
+    .then(result => {
+      res.json(result);
+    }).catch((err) => next(err));
+});
+
+
+
+
+
+// router.post('/', function(req, res, next) {
+//   let data = req.body;
+//   data = Object.keys(data);
+//   data = JSON.parse(data);
+//   data.forEach(p => {
+//     Pdp.insert(p);
+//   });
+//   // let holder = [];
+//   // holder.push(data);
+//   // holder = holder.reduce((a,b) => {
+//   //   return a.concat(b);
+//   // });
+//   // holder.forEach(day => {
+//   //   Pdp.insert(day);
+//   // });
+// });
+
+
+module.exports = router;
