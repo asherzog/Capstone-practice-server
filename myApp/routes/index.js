c60aed93302165c8e4d035ba4cb7921406293d03
Andy Herzog <asherzog19@gmail.com>
2017-03-18 19:43:10 -0600

    updated routes

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -53,11 +53,21 @@ router.delete('/wells/:id', (req, res, next) => {
     }).catch((err) => next(err));
 });
 
-router.get('/pdp', function(req, res, next) {
-  Pdp.count({}).then(response => console.log(response));
-  return Pdp.find({})
-    .then(response => res.json(response))
-    .catch((err) => next(err));
+router.get('/pdp/:id', function(req, res, next) {
+  return Pdp.aggregate([
+    {
+      '$match' : {"Water_System" : req.params.id},
+    },
+    {
+      '$group' : {
+        '_id' : {
+          'Date' : "$OUTDATE"
+        },
+        'total' : {'$sum': "$Gross_Water_Bbls"},
+        'count' : {'$sum': 1}
+      }
+    }
+  ]).then(result => res.json(result));
 });
 router.get('/tc1', function(req, res, next) {
   TC1.count({}).then(response => console.log(response));
@@ -75,6 +85,16 @@ router.get('/tc3', function(req, res, next) {
   TC3.count({}).then(response => console.log(response));
   return TC3.find({})
     .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
+router.get('/pdp/:id', (req, res, next) => {
+  return Pdp.find({"Water_System": 'W' + req.params.id}).then(result => res.json(result))
+    .catch((err) => next(err));
+});
+
+router.get('/pdp', (req, res, next) => {
+  return Pdp.find({}).then(result => res.json(result))
     .catch((err) => next(err));
 });
 
@@ -125,24 +145,30 @@ router.patch('/tc3/:id', (req, res, next) => {
 });
 
 
-
+router.patch('/pdp/:id', (req, res, next) => {
+  return Pdp.update({_id: req.params.id}, {$set: req.body})
+    .then(result => {
+      res.json(result);
+    }).catch((err) => next(err));
+});
 
 
 // router.post('/', function(req, res, next) {
 //   let data = req.body;
 //   data = Object.keys(data);
 //   data = JSON.parse(data);
-//   data.forEach(p => {
-//     Pdp.insert(p);
+//   // data.forEach(p => {
+//   //   Pdp.insert(p);
+//   // });
+//   let holder = [];
+//   holder.push(data);
+//   holder = holder.reduce((a,b) => {
+//     return a.concat(b);
 //   });
-//   // let holder = [];
-//   // holder.push(data);
-//   // holder = holder.reduce((a,b) => {
-//   //   return a.concat(b);
-//   // });
-//   // holder.forEach(day => {
-//   //   Pdp.insert(day);
-//   // });
+//
+//   holder.forEach(day => {
+//     TC3.insert(day);
+//   });
 // });
 
 
