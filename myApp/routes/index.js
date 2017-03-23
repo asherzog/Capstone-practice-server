d9dd99552bf6dd2a2d7f8fe31d7239825f187aa6
Andy Herzog <asherzog19@gmail.com>
2017-03-22 19:04:22 -0600

    new tc and rig routes

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -26,6 +26,18 @@ router.get('/allsystems', function(req, res, next) {
     .catch((err) => next(err));
 });
 
+router.get('/allrigs', function(req, res, next) {
+  return wells.distinct("RIG")
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
+router.get('/alltc', function(req, res, next) {
+  return wells.distinct("TYPE_CURVE")
+    .then(response => res.json(response))
+    .catch((err) => next(err));
+});
+
 router.get('/wells/:id', function(req, res, next) {
   return wells.find({_id: req.params.id})
     .then(response => res.json(response))
